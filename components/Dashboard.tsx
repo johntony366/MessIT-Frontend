9b794c7fab230ba6fe631b989d9759f73ddfa52f
John <johntony366@gmail.com>
2023-01-10 16:43:47 +0530

    cleanup code

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -9,11 +9,6 @@ const Dashboard = () => {
   const router = useRouter();
   console.log(router.query.messDetails);
 
-  useEffect(() => {
-    // @ts-ignore
-    document.querySelector(".scroll-head").style.backgroundColor = "#1a202e;";
-  }, []);
-
   const onSelectedDay = (d: Date) => {
     console.log(d);
   };
@@ -22,9 +17,8 @@ const Dashboard = () => {
     <Box h="100vh" w="100vw" bg="#1a202e" p={12} color="white">
       <DatePicker
         selectedDay={onSelectedDay}
-        enableScroll={false}
-        enableDays={180}
-        enableDaysBefore={2}
+        // enableScroll={false}
+        enableDays={30}
       />
     </Box>
   );
